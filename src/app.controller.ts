2a5be045ed53378ab7949f0a09c6960d8e3e2c95
xinxu <xin872906473@163.com>
2023-06-02 14:45:11 +0800

    了解多种provider写法 灵活注入

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    /** 注入时是字符串的话需要 @Inject('app_service') 手动指定 */ private readonly appService: AppService,
+    @Inject('person1') private readonly person1: { name: string; age: number },
+    @Inject('person2') private readonly person2: { name: string; desc: string },
+    @Inject('person3') private readonly person3: { name: string; desc: string },
+  ) {}
+
+  // 非构造器注入
+  // @Inject(AppService)
+  // private readonly appService: AppService;
 
   @Get()
   getHello(): string {
+    this.person1;
+    this.person2;
+    this.person3;
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,40 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // AppService,  简写
+    {
+      provide: AppService, // 可以是字符串
+      useClass: AppService,
+    }, //完整写法
+    {
+      provide: 'person1',
+      useValue: {
+        name: 'aaa',
+        age: 20,
+      },
+    },
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'bbb',
+          desc: 'cccc',
+        };
+      },
+      // useExisting 设置别名
+    },
+    {
+      provide: 'person3',
+      // 支持异步
+      useFactory(person1: { name: string }, appService: AppService) {
+        return {
+          name: person1.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['person1', AppService],
+    },
+  ],
 })
 export class AppModule {}
